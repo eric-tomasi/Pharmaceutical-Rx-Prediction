da8727758671507cfcbd12cb8553ae8d6c563630
Eric Tomasi <etomasi2323@gmail.com>
2022-07-15 19:27:45 -0400

    Feature: began data exploration and cleaned dataframe

--- model.R
+++ model.R
@@ -15,3 +15,68 @@ library(tidyr)
 
 ######################### Load dataset  #########################
 pharma <- read.csv('pharma.csv')
+
+
+
+######################### Exploratory Data Analysis #########################
+head(pharma)
+summary(pharma)
+
+# Data Preparation and Cleaning
+
+#Removing records with "NATIONAL ASSIGNMENT"
+pharma <- pharma %>% 
+  filter(Last.Name != "NATIONAL ASSIGNMENT") %>% 
+  #dropping TIMOLOL MAL/DORZ HCL/PF since there is no volume
+  filter(Product != "TIMOLOL MAL/DORZ HCL/PF")
+
+#Remove unnecessary columns
+pharma <- pharma %>%
+  select(-c(1,3,4,5,6,7,8,9,14:37,39, 41))
+
+#Rename columns for ease of use
+pharma <- pharma %>%
+  rename(Calls = Number.of.Calls.on.Physician.in.Last.12.Months, 
+         TRx = Total.TRx.Count.for.Last.12.Months)
+
+#Combine Specialties other than Optometrist and Opthalmologist into one group
+pharma <- pharma %>%
+  mutate(Specialty = factor(case_when(!Specialty %in% c("OPHTHALMOLOGY", "OPTOMETRIST") ~ "OTHER", 
+                                      TRUE ~ Specialty)))
+
+#Fill in null values with defaults
+pharma <- pharma %>%
+  mutate(Region = factor(case_when(Region == "" ~ "Whitespace",
+                                   Region == "UNASSIGNED" ~ "Whitespace",
+                                   TRUE ~ Region)),
+         Current.Target = factor(case_when(Current.Target == "" ~ "N",
+                                           TRUE ~ Current.Target)))
+
+
+
+#change datatypes of columns and create factor variables
+pharma <- pharma %>%
+  mutate(IQVIA.ID = as.character(IQVIA.ID), 
+         Product = as.factor(Product))
+
+
+#Sum TRx and max of calls
+pharma <- pharma %>%
+  group_by(IQVIA.ID, Region, Specialty, Current.Target, Product) %>%
+  summarise(Calls = max(Calls),
+            TRx = sum(TRx))
+
+#Pivot products into columns
+pharma <- pharma %>% 
+  pivot_wider(names_from = "Product", values_from = "TRx") %>%
+  as.data.frame(pharma)
+
+
+#Fill NA with 0
+pharma <- pharma %>%
+  replace(is.na(.), 0)
+
+#define response variable (Zioptan) as binary
+pharma <- pharma %>%
+  mutate(response = factor(case_when(ZIOPTAN >= 1 ~ 1, 
+                                     ZIOPTAN <  1 ~ 0)))
